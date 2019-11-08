56ed965d892a150ad5a7a36e01a28b49f5877139
Diana Badas <diana.badas@gmail.com>
2019-11-08 11:55:06 +0000

    Adding LatestBookListcontainer Component

---
+++ src/containers/LatestBookListContainer.js
@@ -0,0 +1,48 @@
+import React, { PureComponent } from 'react';
+import '../BookListContainer.css';
+import BookList from '../components/BookList';
+//import { fetchBooks } from '../actions/bookList';
+
+//import { connect } from 'react-redux';
+
+class SearchableBookListContainer extends PureComponent {
+
+    state = {
+        books: []
+    };
+
+    componentDidMount() {  
+        fetch("https://www.googleapis.com/books/v1/volumes?q=flowers&orderBy=newest")
+        .then(response => {
+            if ( !response.ok ) { throw response }
+            return response.json()  //we only get here if there is no error
+        })
+        .then(books => {
+            console.log(books.items)
+            if (books.error) {
+                alert("There was an error")
+            } else {
+                this.setState({
+                    books: books.items
+                })
+            }
+        })
+        .catch( err => {
+            err.text().then(errorMessage => {
+                alert(JSON.parse(errorMessage).error.message)
+            })
+        })
+    
+    };
+
+    render () {
+        return (
+            <div className="BookListContainer">
+                <BookList books={this.state.books} />
+            </div>
+        );
+    };
+
+};
+
+export default SearchableBookListContainer;
