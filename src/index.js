940431e0a130e50f1f52001df05f9ab8b7447786
Diana Badas <diana.badas@gmail.com>
2019-11-11 14:06:27 +0000

    Commenting everything related to redux before ceating new branch for routes

--- src/index.js
+++ src/index.js
@@ -5,18 +5,18 @@ import LatestBookListContainer from './containers/LatestBookListContainer';
 
 import * as serviceWorker from './serviceWorker';
 
-import { Provider } from 'react-redux'; 
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import booksReducer from './reducers/booksReducer';
+//import { Provider } from 'react-redux'; 
+//import { createStore, applyMiddleware } from 'redux';
+//import thunk from 'redux-thunk';
+//import booksReducer from './reducers/booksReducer';
 
-const store = createStore(booksReducer, applyMiddleware(thunk));
+//const store = createStore(booksReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
-    <Provider store={store} >
-        <SearchableBookListContainer />
-        <LatestBookListContainer />
-    </Provider>, document.getElementById('root')
+        <>
+            <SearchableBookListContainer />
+            <LatestBookListContainer />
+        </>, document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
