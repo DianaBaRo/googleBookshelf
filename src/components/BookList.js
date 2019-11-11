7ed3071a66eb389ce62db5436e009e9eb7b0e6d4
Diana Badas <diana.badas@gmail.com>
2019-11-11 12:35:46 +0000

    Deleting div, using react fragment

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -2,11 +2,9 @@ import React from 'react';
 import Book from './Book';
 
 const BookList = ({ books }) => (
-    <div className="book-list">
-    { books.map(book => <Book title={book.volumeInfo.title} img_url={book.volumeInfo.imageLinks.thumbnail} />) }
-  </div>
-
+    <>
+        { books.map(book => <Book title={book.volumeInfo.title} img_url={book.volumeInfo.imageLinks.thumbnail} />) }
+    </>
 )
-
 
 export default BookList;
