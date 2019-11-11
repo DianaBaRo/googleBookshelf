5e8806ddec49fddf5ab2ae0d520c1ec60f68940e
Diana Badas <diana.badas@gmail.com>
2019-11-11 12:34:54 +0000

    Adding preventDefault to the handleSubmit, validate user input, not using Button component, only a simple HTML button type submit

--- src/containers/SearchableBookListContainer.js
+++ src/containers/SearchableBookListContainer.js
@@ -2,7 +2,7 @@ import React, { PureComponent } from 'react';
 import '../css/BookListContainer.css';
 import BookList from '../components/BookList';
 //import { fetchBooks } from '../actions/bookList';
-import  Button  from '../components/Button';
+//import  Button  from '../components/Button';
 
 //import { connect } from 'react-redux';
 
@@ -16,10 +16,12 @@ class SearchableBookListContainer extends PureComponent {
     handleChange = event =>
         this.setState({ query: event.target.value });
 
-    handleButton = () => {  
+    handleSubmit = event => {  
+        event.preventDefault();
+
         fetch(`https://www.googleapis.com/books/v1/volumes?q=${this.state.query}`)
         .then(response => {
-            if ( !response.ok ) { throw response }
+            if ( !response.ok && this.state.query <= 0 ) { throw response }
             return response.json()  //we only get here if there is no error
         })
         .then(books => {
@@ -43,12 +45,20 @@ class SearchableBookListContainer extends PureComponent {
     render () {
         return (
             <div className="BookListContainer">
-                <form className='searchForm'>
-                    <label><h1>Search for Book</h1></label>
-                    <input id='query' type='text' name='query' onChange={this.handleChange} value={this.state.query}/>
+                <form className='searchForm' onSubmit={this.handleSubmit}>
+                    <label htmlFor='query'><h1>Search for Book</h1></label>
+                    <input 
+                      id='query' 
+                      type='text' 
+                      aria-label='Search for Book' 
+                      aria-required='true' 
+                      name='query' 
+                      onChange={this.handleChange} 
+                      value={this.state.query}/>
+                    <button type='submit'>Search</button>
                 </form>
 
-                { Button(this.handleButton, "Search") }
+                {/* { Button(this.handleButton, "Search") } */}
                 {typeof this.state.books === 'object' && this.state.books.length > 0 && <h2>Books By Search:</h2>}
                 <BookList books={this.state.books} />
             </div>
