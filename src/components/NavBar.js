bdd00941638b2ae420f8c217266dc3df77ad7ab6
Diana Badas <diana.badas@gmail.com>
2019-11-21 14:42:41 +0000

    Adding wishlist and create-book routes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,9 +4,11 @@ import { NavLink } from 'react-router-dom';
 const NavBar = () => {
   return (
     <div style={{ borderBottom: '2px solid black', paddingBottom: '10px', marginBottom: '12px' }}>
-      <NavLink style={{ marginRight: '10px' }}  to="/">Home</NavLink><br />
-      <NavLink style={{ marginRight: '10px' }}  to="/search">Search</NavLink><br />
-      <NavLink style={{ marginRight: '10px' }}  to="/new-releases">New Releases</NavLink>
+        <NavLink style={{ marginRight: '10px' }}  to="/">Home</NavLink><br />
+        <NavLink style={{ marginRight: '10px' }}  to="/search">Search</NavLink><br />
+        <NavLink style={{ marginRight: '10px' }}  to="/new-releases">New Releases</NavLink><br />
+        <NavLink style={{ marginRight: '10px' }} to="/wishlist" >Wishlist </NavLink><br />
+        <NavLink style={{ marginRight: '10px' }} to="/create-book" >Create book </NavLink>
     </div>
   );
 };
