f63d22d07eeafa5b4d96ce02d692ed90faf352b8
Diana Badas <diana.badas@gmail.com>
2019-11-21 17:06:29 +0000

    Refactoring creating const to hold book

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,13 +2,16 @@ import React from 'react';
 import '../css/Book.css';
 
 const Book = (props) => {
-    console.log(props.location.state)
-    if (props.location.state) {
-        let bookTitle = props.location.state.volumeInfo.title;
-        let bookImage = props.location.state.volumeInfo.imageLinks.thumbnail;
-        let bookAuthor = props.location.state.volumeInfo.authors.join('');
-        let bookInfo = props.location.state.volumeInfo.infoLink;
-        let bookDescription = props.location.state.volumeInfo.description;
+
+    const { book } = props.location.state;
+
+    if (book) {
+
+        let bookTitle = book.volumeInfo.title;
+        let bookImage = book.volumeInfo.imageLinks.thumbnail;
+        let bookAuthor = book.volumeInfo.authors.join('');
+        let bookInfo = book.volumeInfo.infoLink;
+        let bookDescription = book.volumeInfo.description;
 
         return(
             <div className="BookListContainer">
@@ -21,9 +24,10 @@ const Book = (props) => {
             </div>
         )
     } else {
+
         return <h1>URL not valid</h1>
 
-    }
+    };
 };
 
 export default Book;
