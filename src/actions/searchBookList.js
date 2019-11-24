185dbd03cc9b1a138f5fde4a8c29a91fc26b81ca
Diana Badas <diana.badas@gmail.com>
2019-11-24 14:25:25 +0000

    Adding error handling and some validations to fetch api

--- src/actions/searchBookList.js
+++ src/actions/searchBookList.js
@@ -2,7 +2,22 @@ export function fetchSearchBookList (query) {
     return ( dispatch ) => {
         dispatch({ type: 'LOADING_SEARCH_BOOK_LIST' });
         fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`)
-        .then( response => response.json() )
-        .then( books => dispatch({ type: 'ADD_SEARCH_BOOK_LIST', books }) );
+        .then( response => {
+            if ( !response.ok && query.length === 0 ) { throw response }
+            return response.json()  //we only get here if there is no error
+        })
+        .then( books => {
+            console.log(books.items)
+            if (books.error) {
+                alert("There was an error")
+            } else {
+                dispatch({ type: 'ADD_SEARCH_BOOK_LIST', books })
+            }
+        })
+        .catch( err => {
+            err.text().then(errorMessage => {
+                alert(JSON.parse(errorMessage).error.message)
+            })
+        })
     };
 };
